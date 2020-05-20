bcdc1e207c48b7930ec8de21048ed972f045e284
EC2 Default User <ec2-user@ip-172-31-92-222.ec2.internal>
2020-05-20 21:40:50 +0000

    add obj

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
+var port = process.env.PORT || 8080;
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
+
+var obj =   [{"id":11, "title":"hello", "body":"world"}]
+
 
 app.get('/', function(req, res) {
-  res.send({
-    "Output" : "Hello World!"
-  });
+  res.send(
+    JSON.stringify(obj)
+  );
 });
 
 app.post('/', function(req, res) {
